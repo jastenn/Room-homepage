e0009062802c6acc7cfa9f32c2418b65d3cae292
Justine Aviso <justine.aviso3@gmail.com>
2022-03-18 15:11:52 +0800

    tab order fixed and script encapsulated

--- js/carousel.js
+++ js/carousel.js
@@ -1,116 +1,125 @@
-const carouselBtnPrev = document.querySelector('.carousel__controls--prev')
-const carouselBtnNext = document.querySelector('.carousel__controls--next')
-const carauselItems = document.querySelectorAll('.carousel__item')
-const heroTextItems = document.querySelectorAll('.header-list__item')
-let carouselActiveIdx = 0 //zero based
-const carouselLastIdx = carauselItems.length - 1
-let heroTextActiveIdx = 0 //zero based
-const heroTextLastIdx = heroTextItems.length - 1
+(function () {
+  const carouselBtnPrev = document.querySelector(".carousel__controls--prev");
+  const carouselBtnNext = document.querySelector(".carousel__controls--next");
+  const carauselItems = document.querySelectorAll(".carousel__item");
+  const heroTextItems = document.querySelectorAll(".header-list__item");
+  let carouselActiveIdx = 0; //zero based
+  const carouselLastIdx = carauselItems.length - 1;
+  let heroTextActiveIdx = 0; //zero based
+  const heroTextLastIdx = heroTextItems.length - 1;
 
-carouselBtnPrev.addEventListener('click', carouselPrevBtnHandler)
-carouselBtnNext.addEventListener('click', carouselNextBtnHandler)
+  carouselBtnPrev.addEventListener("click", carouselPrevBtnHandler);
+  carouselBtnNext.addEventListener("click", carouselNextBtnHandler);
 
-window.addEventListener('keyup', (e) => {
-  if(!carouselBtnPrev.disabled && !carouselBtnNext.disabled) {
-    if(e.key === 'ArrowLeft') {
-      carouselPrevBtnHandler()
-    } else if(e.key == 'ArrowRight') {
-      carouselNextBtnHandler()
+  window.addEventListener("keyup", (e) => {
+    if (!carouselBtnPrev.disabled && !carouselBtnNext.disabled) {
+      if (e.key === "ArrowLeft") {
+        carouselPrevBtnHandler();
+      } else if (e.key == "ArrowRight") {
+        carouselNextBtnHandler();
+      }
     }
-  }
-})
+  });
 
-function carouselPrevBtnHandler() {
-  //Vars
-  const curActiveCarouselItem = carauselItems[carouselActiveIdx]
-  const curActiveHeroTextItem = heroTextItems[heroTextActiveIdx]
-  
-  //for carousel img
-  curActiveCarouselItem.classList.add('carousel__item--leavingLeft')
-  curActiveCarouselItem.classList.remove('carousel__item--active')
+  function carouselPrevBtnHandler() {
+    //Vars
+    const curActiveCarouselItem = carauselItems[carouselActiveIdx];
+    const curActiveHeroTextItem = heroTextItems[heroTextActiveIdx];
 
-  //for hero text
-  curActiveHeroTextItem.classList.add('header-list__item--leaving')
-  curActiveHeroTextItem.classList.remove('header-list__item--active')
-  setTimeout(() => {
-    //for hero text
-    curActiveHeroTextItem.classList.remove('header-list__item--leaving')
-  }, 300);
-  setTimeout(() => {
     //for carousel img
-    curActiveCarouselItem.classList.remove('carousel__item--leavingLeft')
-  }, 600);
+    curActiveCarouselItem.classList.add("carousel__item--leavingLeft");
+    curActiveCarouselItem.classList.remove("carousel__item--active");
 
-  carouselActiveIdx <= 0 ? carouselActiveIdx = carouselLastIdx : carouselActiveIdx --
-  heroTextActiveIdx <= 0 ? heroTextActiveIdx = heroTextLastIdx : heroTextActiveIdx --
-  const nextActiveCarouselItem = carauselItems[carouselActiveIdx]
-  const nextActiveHeroTextItem = heroTextItems[carouselActiveIdx]
-  //for carausel Img
-  carouselBtnPrev.disabled = true
-  carouselBtnNext.disabled = true
-  nextActiveCarouselItem.classList.add('carausel__item--fromRight')
- 
-
-  setTimeout(() => {
     //for hero text
-    nextActiveHeroTextItem.classList.add('header-list__item--next')
-  }, 300);
+    curActiveHeroTextItem.classList.add("header-list__item--leaving");
+    curActiveHeroTextItem.classList.remove("header-list__item--active");
+    setTimeout(() => {
+      //for hero text
+      curActiveHeroTextItem.classList.remove("header-list__item--leaving");
+    }, 300);
+    setTimeout(() => {
+      //for carousel img
+      curActiveCarouselItem.classList.remove("carousel__item--leavingLeft");
+    }, 600);
 
-  setTimeout(() => {
-    //for hero text
-    nextActiveHeroTextItem.classList.remove('header-list__item--next')
-    nextActiveHeroTextItem.classList.add('header-list__item--active')
-
+    carouselActiveIdx <= 0
+      ? (carouselActiveIdx = carouselLastIdx)
+      : carouselActiveIdx--;
+    heroTextActiveIdx <= 0
+      ? (heroTextActiveIdx = heroTextLastIdx)
+      : heroTextActiveIdx--;
+    const nextActiveCarouselItem = carauselItems[carouselActiveIdx];
+    const nextActiveHeroTextItem = heroTextItems[carouselActiveIdx];
     //for carausel Img
-    nextActiveCarouselItem.classList.remove('carausel__item--fromRight')
-    nextActiveCarouselItem.classList.add('carousel__item--active')
-    carouselBtnPrev.disabled = false
-    carouselBtnNext.disabled = false
-  }, 600);
-}
+    carouselBtnPrev.disabled = true;
+    carouselBtnNext.disabled = true;
+    nextActiveCarouselItem.classList.add("carausel__item--fromRight");
 
-function carouselNextBtnHandler() {
-  //Vars
-  const curActiveItem = carauselItems[carouselActiveIdx]
-  const curActiveHeroTextItem = heroTextItems[heroTextActiveIdx]
-  // carauselItems[carouselActiveIdx + 1].classList.add('carousel__item--origin-right')
-  curActiveItem.classList.add('carousel__item--leavingRight')
-  curActiveItem.classList.remove('carousel__item--active')
-  //for hero text
-  curActiveHeroTextItem.classList.add('header-list__item--leaving')
-  curActiveHeroTextItem.classList.remove('header-list__item--active')
-  setTimeout(() => {
-    curActiveHeroTextItem.classList.remove('header-list__item--leaving')
-  }, 300);
+    setTimeout(() => {
+      //for hero text
+      nextActiveHeroTextItem.classList.add("header-list__item--next");
+    }, 300);
 
-  //for carousel img
-  setTimeout(() => {
-    curActiveItem.classList.remove('carousel__item--leavingRight')
-  }, 600);
+    setTimeout(() => {
+      //for hero text
+      nextActiveHeroTextItem.classList.remove("header-list__item--next");
+      nextActiveHeroTextItem.classList.add("header-list__item--active");
 
-  carouselActiveIdx >= carouselLastIdx ? carouselActiveIdx = 0 : carouselActiveIdx ++
-  heroTextActiveIdx >= heroTextLastIdx ? heroTextActiveIdx = 0 : heroTextActiveIdx ++
-  //Vars
-  const nextActiveCarouselItem = carauselItems[carouselActiveIdx]
-  const nextActiveHeroTextItem = heroTextItems[carouselActiveIdx]
+      //for carausel Img
+      nextActiveCarouselItem.classList.remove("carausel__item--fromRight");
+      nextActiveCarouselItem.classList.add("carousel__item--active");
+      carouselBtnPrev.disabled = false;
+      carouselBtnNext.disabled = false;
+    }, 600);
+  }
 
-  nextActiveCarouselItem.classList.add('carausel__item--fromLeft')
-  carouselBtnPrev.disabled = true
-  carouselBtnNext.disabled = true
-  setTimeout(() => {
+  function carouselNextBtnHandler() {
+    //Vars
+    const curActiveItem = carauselItems[carouselActiveIdx];
+    const curActiveHeroTextItem = heroTextItems[heroTextActiveIdx];
+    // carauselItems[carouselActiveIdx + 1].classList.add('carousel__item--origin-right')
+    curActiveItem.classList.add("carousel__item--leavingRight");
+    curActiveItem.classList.remove("carousel__item--active");
     //for hero text
-    nextActiveHeroTextItem.classList.add('header-list__item--next')
-  }, 300);
-  setTimeout(() => {
-    //for hero img
-    nextActiveHeroTextItem.classList.remove('header-list__item--next')
-    nextActiveHeroTextItem.classList.add('header-list__item--active')
+    curActiveHeroTextItem.classList.add("header-list__item--leaving");
+    curActiveHeroTextItem.classList.remove("header-list__item--active");
+    setTimeout(() => {
+      curActiveHeroTextItem.classList.remove("header-list__item--leaving");
+    }, 300);
 
     //for carousel img
-    nextActiveCarouselItem.classList.remove('carausel__item--fromLeft')
-    nextActiveCarouselItem.classList.add('carousel__item--active')
+    setTimeout(() => {
+      curActiveItem.classList.remove("carousel__item--leavingRight");
+    }, 600);
 
-    carouselBtnPrev.disabled = false
-    carouselBtnNext.disabled = false
-  }, 600);
-}
+    carouselActiveIdx >= carouselLastIdx
+      ? (carouselActiveIdx = 0)
+      : carouselActiveIdx++;
+    heroTextActiveIdx >= heroTextLastIdx
+      ? (heroTextActiveIdx = 0)
+      : heroTextActiveIdx++;
+    //Vars
+    const nextActiveCarouselItem = carauselItems[carouselActiveIdx];
+    const nextActiveHeroTextItem = heroTextItems[carouselActiveIdx];
+
+    nextActiveCarouselItem.classList.add("carausel__item--fromLeft");
+    carouselBtnPrev.disabled = true;
+    carouselBtnNext.disabled = true;
+    setTimeout(() => {
+      //for hero text
+      nextActiveHeroTextItem.classList.add("header-list__item--next");
+    }, 300);
+    setTimeout(() => {
+      //for hero img
+      nextActiveHeroTextItem.classList.remove("header-list__item--next");
+      nextActiveHeroTextItem.classList.add("header-list__item--active");
+
+      //for carousel img
+      nextActiveCarouselItem.classList.remove("carausel__item--fromLeft");
+      nextActiveCarouselItem.classList.add("carousel__item--active");
+
+      carouselBtnPrev.disabled = false;
+      carouselBtnNext.disabled = false;
+    }, 600);
+  }
+})();
--- js/index.js
+++ js/index.js
@@ -1,44 +1,46 @@
-const hamburger = document.querySelector('.nav__hamburger')
-const closeNav = document.querySelector('.nav__close-nav')
-const navList = document.querySelector('.nav__list')
-const topNavContainer = navList.parentElement
-const navBackdrop = document.getElementById('backdrop')
+(function () {
+  const hamburger = document.querySelector('.nav__hamburger')
+  const closeNav = document.querySelector('.nav__close-nav')
+  const navList = document.querySelector('.nav__list')
+  const topNavContainer = navList.parentElement
+  const navBackdrop = document.getElementById('backdrop')
 
-hamburger.addEventListener('click', hamburgerHandler)
-closeNav.addEventListener('click', closeNavHandler)
-navBackdrop.addEventListener('click', closeNavHandler)
-window.addEventListener('resize', resizeHandler)
+  hamburger.addEventListener('click', hamburgerHandler)
+  closeNav.addEventListener('click', closeNavHandler)
+  navBackdrop.addEventListener('click', closeNavHandler)
+  window.addEventListener('resize', resizeHandler)
 
-function hamburgerHandler() {
-  topNavContainer.classList.add('active')
-  navBackdrop.style.display = 'block'
-  document.body.style.height = '100vh'
-  document.body.style.overflowY = 'hidden'
-  setTimeout(() => {
-    backdropHandler()
-  }, 150);
-}
-
-function closeNavHandler() {
-  topNavContainer.classList.remove('active')
-  document.body.removeAttribute('style')
-  setTimeout(() => backdropHandler(), 100);
-  setTimeout(() => navBackdrop.removeAttribute('style'), 300);
-}
-
-function backdropHandler() {
-  if(topNavContainer.classList.contains('active')) {
-    navBackdrop.classList.add('active')
-  } else {
-    navBackdrop.classList.remove('active')
+  function hamburgerHandler() {
+    topNavContainer.classList.add('active')
+    navBackdrop.style.display = 'block'
+    document.body.style.height = '100vh'
+    document.body.style.overflowY = 'hidden'
+    setTimeout(() => {
+      backdropHandler()
+    }, 150);
   }
-}
 
-function resizeHandler(e) {
-  if(e.target.innerWidth >= 800) {
+  function closeNavHandler() {
+    topNavContainer.classList.remove('active')
+    document.body.removeAttribute('style')
+    setTimeout(() => backdropHandler(), 100);
+    setTimeout(() => navBackdrop.removeAttribute('style'), 300);
+  }
+
+  function backdropHandler() {
     if(topNavContainer.classList.contains('active')) {
-      topNavContainer.classList.remove('active')
+      navBackdrop.classList.add('active')
+    } else {
       navBackdrop.classList.remove('active')
     }
   }
-}
+
+  function resizeHandler(e) {
+    if(e.target.innerWidth >= 800) {
+      if(topNavContainer.classList.contains('active')) {
+        topNavContainer.classList.remove('active')
+        navBackdrop.classList.remove('active')
+      }
+    }
+  }
+})()
